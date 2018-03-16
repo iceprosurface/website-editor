3f9e095d61039a83a80bc2bd0a79e8f29c47c488
iceprosurface <775846939@qq.com>
2018-03-16 14:28:17 +0800

    Feature/master doc build (#6)
    * [feature] 优化了guildline的显示
    
    * [doc] doc build

--- src/nativePlugins/plugins/btn/button-instance.vue
+++ src/nativePlugins/plugins/btn/button-instance.vue
@@ -16,5 +16,10 @@ export default {
 <style lang="less" type="text/less">
   .button-instance {
     display: inline-block;
+    button {
+      border: 1px solid #e3e3e3;
+      border-radius: 0;
+      background: none;
+    }
   }
 </style>
--- src/nativePlugins/plugins/container/container-instance.vue
+++ src/nativePlugins/plugins/container/container-instance.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container" :style="computedStyle"  data-slot-name='default'>
-      <div class="instance-name">{{this.instanceKey}}</div>
+      <div class="instance-name" :class="{hover: currentHoverInstanceKey === instanceKey}">{{this.instanceKey}}</div>
       <slot></slot>
   </div>
 </template>
@@ -22,25 +22,46 @@ export default {
         minHeight: '100px',
         minWidth: '100px'
       }
+    },
+    currentHoverInstanceKey () {
+      return this.$store.state.viewport.currentHoverInstanceKey
     }
   }
 }
 </script>
 <style lang="less" type="text/less">
+  @border: #ddd;
   .container {
-    border: 1px dotted #ddd;
+    border: 1px dotted ;
     padding: 1px;
     position: relative;
     @enhance-color: #7d7d7d;
     &.sortable-drag, &.sortable-ghost {
       border: 1px solid @enhance-color;
+      box-shadow:2px 2px 3px #aaaaaa;
       & > .instance-name {
         display: block;
+        border-left: 1px solid @enhance-color;
+        border-bottom: 1px solid @enhance-color;
+        &.hover {
+          opacity: 1;
+          border-left: 1px solid @enhance-color;
+          border-bottom: 1px solid @enhance-color;
+        }
       }
     }
     .instance-name {
-      display: none;
+      &.hover {
+        opacity: 1;
+        border-left: 1px solid #23b7e5;
+        border-bottom: 1px solid #23b7e5;
+      }
+      transition: opacity 0.35s ease;
+      user-select: none;
+      z-index: 1000;
+      background: #ffffff;
       position: absolute;
+      opacity: 0;
       right: 0;
       padding: 4px;
       font-size: 12px;
